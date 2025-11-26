687ff615044f54e0206866f372cc4d37eae7cb30
Dylan <djlevy26@gmail.com>
2025-11-26 17:36:08 -0500

    test 4

--- custom_components/rainbow_lights/__init__.py
+++ custom_components/rainbow_lights/__init__.py
@@ -1,4 +1,22 @@
 from .const import DOMAIN
 
+PLATFORMS = ["switch"]
+
+
 async def async_setup_entry(hass, entry):
+    """Set up Rainbow Lights from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Forward setup to platforms (switch)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+    return unload_ok
